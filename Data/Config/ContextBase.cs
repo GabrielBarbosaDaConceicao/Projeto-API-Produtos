9ea9b650167aa155ec0c57a08bdc1fe9729c9f45
Gabriel Barbosa Da Conceição <gabriel.bc.dev@hotmail.com>
2022-02-04 14:36:24 -0300

    Primari commit

---
+++ Data/Config/ContextBase.cs
@@ -0,0 +1,46 @@
+﻿using Data.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        { 
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Sourse=DESKTOP-FPH60V2\\DESENVOLVIMENTO;Initial catalog=PROJETO_api;Integrated Security=False;User ID=sa;Password=GabrielBc5891;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+
+            return strCon;
+        }
+
+
+    }
+}

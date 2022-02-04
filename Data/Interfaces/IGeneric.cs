3b9d9e9ea2f6396708b681006ba5c3a4d9063148
Gabriel Barbosa Da Conceição <gabriel.conceicao@zarpsystem.com.br>
2022-02-04 15:33:41 -0300

    Add Interfaces

--- Data/Interfaces/IGeneric.cs
+++ Data/Interfaces/IGeneric.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Data.Interfaces
 {
-    class IGeneric
+    public interface IGeneric<T> where T : class
     {
+        Task Add(T Objeto);
+        Task Update(T Objeto);
+        Task Delete(T Objeto);
+        Task<T> GwetEntityById(int Id);
+        Task<List<T>> list();
     }
 }
---
+++ Data/Repositorio/RepositoryGenerics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositorio
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+
+    }
+    
+}
